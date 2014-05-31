14b46799121f2502e1f1a8990dfe9844d88a1f55
David Paquette <david.paquette@usask.ca>
2014-05-31 17:10:02 -0500

    Revert "Revert "Initial Commit""
    This reverts commit 9ba22dcf887e923543c0adda2a4bf69c6d963cea.

---
+++ SeedingDataFromCSV/Domain/LocationContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace SeedingDataFromCSV.Domain
+{
+public class LocationContext : DbContext
+{
+    public IDbSet<Country> Countries { get; set; }
+    public IDbSet<ProvinceState> ProvinceStates { get; set; }
+
+}
+}
---
+++ SeedingDataFromCSV/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SeedingDataFromCSV.Startup))]
+namespace SeedingDataFromCSV
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
